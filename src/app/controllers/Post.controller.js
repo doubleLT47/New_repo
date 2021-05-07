150fbf187df44ceb05a4932054fd13e5d05e0c3c
doubleTL47 <luandz.6147@gmail.com>
2021-05-08 01:37:56 +0700

    render post

--- src/app/controllers/Post.controller.js
+++ src/app/controllers/Post.controller.js
@@ -1,5 +1,6 @@
 
 const postModel = require('../model/posts');
+const UserAccount = require('../model/userAccount');
 
 class Post {
     //[GET] /index
@@ -25,9 +26,29 @@ class Post {
 
     showListPosts(req, res, next) {
         postModel.find({})
-        .then(posts => res.send(JSON.stringify(posts)))
-        .catch((err)=> res.status(500).json({err: err}));
+            .then(posts => {
+                let data = [];
+                let i = posts.length;
+                posts.map(async (post, index) => {
+                    let acc = await UserAccount.findOne({_id: post.userID});
+                    
+                    let {_id, caption,image, thematic, userID, createAt} = post;
+                    
+                    let userName = acc.fullname, userAvatar = acc.avatar;
+                    var obj = {_id, caption, image, thematic, userID, createAt, userName, userAvatar};
+                    data.push(obj); 
+                    if (index === i - 1) {
+                        res.send(JSON.stringify(data));
+                    }
+                })
+                
+                
+            })
+            .catch((err)=> res.status(500).json({err: "Server not responding!"}));
     }
+        
 }
+
+
 
 module.exports = new Post;
